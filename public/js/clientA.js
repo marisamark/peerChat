bd43f01e18ede6583ae46f523e7828a5933775ce
Chris Flick <chrisflick@gmail.com>
2019-10-30 11:43:52 -0700

    Intigrated chat.html into clientA and B .html

--- public/js/clientA.js
+++ public/js/clientA.js
@@ -1,3 +1,4 @@
+
 let name = localStorage.getItem("rm_name")
 let log = ""
 
@@ -34,6 +35,14 @@ peer.on('open', function (id) {
           let newID = res[0].user2Id.split(`"`)
           console.log(newID)
           initChat(newID[1])
+
+          $.ajax({
+            method: 'DELETE',
+            url: `api/lobbies/`,
+            where: {
+              id: id,
+            }
+          })
         }
       })
     }, 1000)
--- public/js/clientB.js
+++ public/js/clientB.js
@@ -1,5 +1,7 @@
 let log = ""
 let userID = localStorage.getItem('PeerChatID');
+let nam = localStorage.getItem('rm_name')
+
 
 var peer = new Peer(makeid(10), {
   host: '74.207.252.238',
@@ -8,6 +10,8 @@ var peer = new Peer(makeid(10), {
 });
 
 let conn;
+
+$('#lobbyName').text(name);
 
   peer.on('open', function (id) {
     console.log('Initializing PeerJS: ' + id);
